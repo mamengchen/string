46e1a42cd60e572e4ac41a429f23825a9970c7fa
mamengchen <624718596@qq.com>
2019-05-03 23:23:59 +0800

    轮船调度

--- 数学建模/轮船摆渡.cpp
+++ 数学建模/轮船摆渡.cpp
@@ -50,11 +50,13 @@ struct Boat_queue
 
 void meun()
 {
-	printf("----\t1.如果轮船公司只有1艘游轮，问该轮船如何安排航程？一天总载客量是多少?\t-----\n");
-	printf("----\t2.若轮船公司有多艘轮船，问轮船公司最少需使用几艘游轮？分别如何安排航程？每艘船载客量是多少？\t-----\n");
-	printf("----\t3.假设游客到达港口最多等待10分钟，若10分钟游轮未到，则自动离开。请在该假设下重新考虑问题1\t-----\n");
-	printf("----\t4.假设游客到达港口最多等待10分钟，若10分钟游轮未到，则自动离开。请在该假设下重新考虑问题2\t-----\n");
-	printf("----\t\t 0.exit \t\t-----\n");
+	printf("-----------------------------------------------------------------------------------------------------\n");
+	printf("|1.如果轮船公司只有1艘游轮，问该轮船如何安排航程？一天总载客量是多少?                               |\n");
+	printf("|2.若轮船公司有多艘轮船，问轮船公司最少需使用几艘游轮？分别如何安排航程？每艘船载客量是多少？       |\n");
+	printf("|3.假设游客到达港口最多等待10分钟，若10分钟游轮未到，则自动离开。请在该假设下重新考虑问题?          |\n");
+	printf("|4.假设游客到达港口最多等待10分钟，若10分钟游轮未到，则自动离开。请在该假设下重新考虑问题?          |\n");
+	printf("|                                        0.exit                                                     |\n");
+	printf("-----------------------------------------------------------------------------------------------------\n");
 }
 
 void one_boat_muchtime()
@@ -226,7 +228,7 @@ void wait_one_boat_muchtime()
 		printf("文件打开失败!!!\n");
 	}
 
-	for (int i = 90; i <= PERSON_MAX; i++)
+	for (int i = 90; i <= PERSON_MAX; i += 3)
 	{
 		que.boat->chang_count(0);
 		que.boat->chang_person(i);
@@ -245,16 +247,16 @@ void wait_one_boat_muchtime()
 			que.boat->add_count();
 			person_number = (data + (que.boat->_count - 1)*i);
 			all_monery = person_number * person_monery - que.boat->_count * boat_monery;
-			printf("每次游轮上有[%d]个人，最后一次尽可能多带一些则游轮上有[%d]个人，总计人数<%d>---总利润为:%d;\n", i, data, person_number, all_monery);
-			sprintf(buffer, "每次游轮上有[%d]个人，最后一次尽可能多带一些则游轮上有[%d]个人，总计人数<%d>---总利润为:%d;\n", i, data, person_number, all_monery);
+			printf("每次游轮上有[%d]个人，最后一次尽可能多带一些则游轮上有[%d]个人，游船出行次数{%d}，总计人数<%d>---总利润为:%d;\n", i, data, que.boat->_count, person_number, all_monery);
+			sprintf(buffer, "每次游轮上有[%d]个人，最后一次尽可能多带一些则游轮上有[%d]个人，游船出行次数{%d}，总计人数<%d>---总利润为:%d;\n", i, data, que.boat->_count, person_number, all_monery);
 			fwrite(&buffer, strlen(buffer), 1, fp);
 		}
 		else
 		{
 			person_number = que.boat->_count * i;
 			all_monery = person_number * person_monery - que.boat->_count * boat_monery;
-			printf("每次游轮上有[%d]个人，船在岸边但是不够90人，总计人数<%d>---总利润为:%d;\n", i, person_number, all_monery);
-			sprintf(buffer, "每次游轮上有[%d]个人，船在岸边但是不够90人，总计人数<%d>---总利润为:%d;\n", i, person_number, all_monery);
+			printf("每次游轮上有[%d]个人，船在岸边但是不够90人，游船出行次数{%d}，总计人数<%d>---总利润为:%d;\n", i, que.boat->_count, person_number, all_monery);
+			sprintf(buffer, "每次游轮上有[%d]个人，船在岸边但是不够90人，游船出行次数{%d}，总计人数<%d>---总利润为:%d;\n", i, que.boat->_count, person_number, all_monery);
 			fwrite(&buffer, strlen(buffer), 1, fp);
 		}
 	}
@@ -263,7 +265,7 @@ void wait_one_boat_muchtime()
 
 void wait_much_boat_muchtime()
 {
-	Boat_queue(3);
+	Boat_queue que(3);
 	int boat_monery = 0;
 	int person_monery = 0;
 	int all_monery;
@@ -279,6 +281,231 @@ void wait_much_boat_muchtime()
 	{
 		printf("文件打开失败!!!\n");
 	}
+	for (int i = 90; i <= PERSON_MAX; i += 3)
+	{
+		for (int j = 90; j <= PERSON_MAX; j += 3)
+		{
+			for (int z = 90; z <= PERSON_MAX; z += 3)
+			{
+				que.boat[0].chang_person(i);
+				que.boat[0].chang_count(0);
+				que.boat[0].chang_Boatcost(boat_monery);
+				que.boat[1].chang_person(j);
+				que.boat[1].chang_count(0);
+				que.boat[1].chang_Boatcost(boat_monery);
+				que.boat[2].chang_person(z);
+				que.boat[2].chang_count(0);
+				que.boat[2].chang_Boatcost(boat_monery);
+				int person_number = 0;
+				int boat1_time = i / 3;
+				int boat2_time = j / 3;
+				int boat3_time = z / 3;
+				int all_time = ALL_TIME;
+				int end_person = 0;
+				int wait_time = 90;
+				all_time = all_time - boat1_time - boat2_time - boat3_time + 10;
+				que.boat[0].add_count();
+				que.boat[1].add_count();
+				que.boat[2].add_count();
+				if ((boat1_time + boat2_time) >= 80 && (boat2_time + boat3_time) >= 80 && (boat3_time + boat1_time) >= 80)
+				{
+					while (all_time >= 50)
+					{
+						if (all_time > boat1_time)
+						{
+							que.boat[0].add_count();
+							all_time -= boat1_time;
+						}
+						if (all_time > boat2_time)
+						{
+							que.boat[1].add_count();
+							all_time -= boat2_time;
+						}
+						if (all_time > boat3_time)
+						{
+							que.boat[2].add_count();
+							all_time -= boat3_time;
+						}
+					}
+					if (all_time >= 30)
+					{
+						end_person = all_time * 3;
+					}
+					person_number = que.boat[0]._count * i + que.boat[1]._count * j + que.boat[2]._count * z + end_person;
+					if (end_person)
+					{
+						if (que.boat[0]._count > que.boat[1]._count)
+						{
+							que.boat[1].add_count();
+						}
+						else if (que.boat[1]._count > que.boat[2]._count)
+						{
+							que.boat[2].add_count();
+						}
+						else
+						{
+							que.boat[0].add_count();
+						}
+					}
+					int boat_num = que.boat[0]._count + que.boat[1]._count + que.boat[2]._count;
+					all_monery = person_number * person_monery - boat_monery * boat_num;
+					printf("第一艘游轮上有[%d]个人，第二艘游轮上有[%d]个人，第三艘游轮上有[%d]个人，第一个游轮出行次数{%d}，第二个游轮出行次数{%d}，第三个游轮出行次数{%d}，最后一次上了[%d]个人，总计人数<%d>---总利润为:%d;\n", i, j, z, que.boat[0]._count, que.boat[1]._count, que.boat[2]._count, end_person, person_number, all_monery);
+					sprintf(buffer, "第一艘游轮上有[%d]个人，第二艘游轮上有[%d]个人，第三艘游轮上有[%d]个人，第一个游轮出行次数{%d}，第二个游轮出行次数{%d}，第三个游轮出行次数{%d}，最后一次上了[%d]个人，总计人数<%d>---总利润为:%d;\n", i, j, z, que.boat[0]._count, que.boat[1]._count, que.boat[2]._count, end_person, person_number, all_monery);
+					fwrite(&buffer, strlen(buffer), 1, fp);
+				}
+				else
+				{
+
+					if ((boat1_time + boat2_time) >= 80)
+					{
+						if ((boat2_time + boat3_time) >= 80)
+						{
+							while (all_time >= 50)
+							{
+								if (all_time > boat1_time)
+								{
+									que.boat[0].add_count();
+									all_time -= boat1_time;
+								}
+								if (all_time > boat2_time)
+								{
+									que.boat[1].add_count();
+									all_time -= boat2_time;
+									all_time = all_time - (90 - boat1_time - boat3_time - 10);
+								}
+								if (all_time > boat3_time)
+								{
+									que.boat[2].add_count();
+									all_time -= boat3_time;
+								}
+							}
+							if (all_time >= 30)
+							{
+								end_person = all_time * 3;
+							}
+							person_number = que.boat[0]._count * i + que.boat[1]._count * j + que.boat[2]._count * z + end_person;
+							if (end_person)
+							{
+								if (que.boat[0]._count > que.boat[1]._count)
+								{
+									que.boat[1].add_count();
+								}
+								else if (que.boat[1]._count > que.boat[2]._count)
+								{
+									que.boat[2].add_count();
+								}
+								else
+								{
+									que.boat[0].add_count();
+								}
+							}
+							int boat_num = que.boat[0]._count + que.boat[1]._count + que.boat[2]._count;
+							all_monery = person_number * person_monery - boat_monery * boat_num;
+							printf("第一艘游轮上有[%d]个人，第二艘游轮上有[%d]个人，第三艘游轮上有[%d]个人，第一个游轮出行次数{%d}，第二个游轮出行次数{%d}，第三个游轮出行次数{%d}，最后一次上了[%d]个人，总计人数<%d>---总利润为:%d;\n", i, j, z, que.boat[0]._count, que.boat[1]._count, que.boat[2]._count, end_person, person_number, all_monery);
+							sprintf(buffer, "第一艘游轮上有[%d]个人，第二艘游轮上有[%d]个人，第三艘游轮上有[%d]个人，第一个游轮出行次数{%d}，第二个游轮出行次数{%d}，第三个游轮出行次数{%d}，最后一次上了[%d]个人，总计人数<%d>---总利润为:%d;\n", i, j, z, que.boat[0]._count, que.boat[1]._count, que.boat[2]._count, end_person, person_number, all_monery);
+							fwrite(&buffer, strlen(buffer), 1, fp);
+						}
+						else
+						{
+							while (all_time >= 50)
+							{
+								if (all_time > boat1_time)
+								{
+									que.boat[0].add_count();
+									all_time -= boat1_time;
+									all_time = all_time - (90 - boat2_time - boat3_time - 10);
+								}
+								if (all_time > boat2_time)
+								{
+									que.boat[1].add_count();
+									all_time -= boat2_time;
+								}
+								if (all_time > boat3_time)
+								{
+									que.boat[2].add_count();
+									all_time -= boat3_time;
+								}
+							}
+							if (all_time >= 30)
+							{
+								end_person = all_time * 3;
+							}
+							person_number = que.boat[0]._count * i + que.boat[1]._count * j + que.boat[2]._count * z + end_person;
+							if (end_person)
+							{
+								if (que.boat[0]._count > que.boat[1]._count)
+								{
+									que.boat[1].add_count();
+								}
+								else if (que.boat[1]._count > que.boat[2]._count)
+								{
+									que.boat[2].add_count();
+								}
+								else
+								{
+									que.boat[0].add_count();
+								}
+							}
+							int boat_num = que.boat[0]._count + que.boat[1]._count + que.boat[2]._count;
+							all_monery = person_number * person_monery - boat_monery * boat_num;
+							printf("第一艘游轮上有[%d]个人，第二艘游轮上有[%d]个人，第三艘游轮上有[%d]个人，第一个游轮出行次数{%d}，第二个游轮出行次数{%d}，第三个游轮出行次数{%d}，最后一次上了[%d]个人，总计人数<%d>---总利润为:%d;\n", i, j, z, que.boat[0]._count, que.boat[1]._count, que.boat[2]._count, end_person, person_number, all_monery);
+							sprintf(buffer, "第一艘游轮上有[%d]个人，第二艘游轮上有[%d]个人，第三艘游轮上有[%d]个人，第一个游轮出行次数{%d}，第二个游轮出行次数{%d}，第三个游轮出行次数{%d}，最后一次上了[%d]个人，总计人数<%d>---总利润为:%d;\n", i, j, z, que.boat[0]._count, que.boat[1]._count, que.boat[2]._count, end_person, person_number, all_monery);
+							fwrite(&buffer, strlen(buffer), 1, fp);
+						}
+					}
+					else
+					{
+						while (all_time >= 50)
+						{
+							if (all_time > boat1_time)
+							{
+								que.boat[0].add_count();
+								all_time -= boat1_time;
+							}
+							if (all_time > boat2_time)
+							{
+								que.boat[1].add_count();
+								all_time -= boat2_time;
+							}
+							if (all_time > boat3_time)
+							{
+								que.boat[2].add_count();
+								all_time -= boat3_time;
+								all_time = all_time - (90 - boat1_time - boat2_time - 10);
+							}
+						}
+						if (all_time >= 30)
+						{
+							end_person = all_time * 3;
+						}
+						person_number = que.boat[0]._count * i + que.boat[1]._count * j + que.boat[2]._count * z + end_person;
+						if (end_person)
+						{
+							if (que.boat[0]._count > que.boat[1]._count)
+							{
+								que.boat[1].add_count();
+							}
+							else if (que.boat[1]._count > que.boat[2]._count)
+							{
+								que.boat[2].add_count();
+							}
+							else
+							{
+								que.boat[0].add_count();
+							}
+						}
+						int boat_num = que.boat[0]._count + que.boat[1]._count + que.boat[2]._count;
+						all_monery = person_number * person_monery - boat_monery * boat_num;
+						printf("第一艘游轮上有[%d]个人，第二艘游轮上有[%d]个人，第三艘游轮上有[%d]个人，第一个游轮出行次数{%d}，第二个游轮出行次数{%d}，第三个游轮出行次数{%d}，最后一次上了[%d]个人，总计人数<%d>---总利润为:%d;\n", i, j, z, que.boat[0]._count, que.boat[1]._count, que.boat[2]._count, end_person, person_number, all_monery);
+						sprintf(buffer, "第一艘游轮上有[%d]个人，第二艘游轮上有[%d]个人，第三艘游轮上有[%d]个人，第一个游轮出行次数{%d}，第二个游轮出行次数{%d}，第三个游轮出行次数{%d}，最后一次上了[%d]个人，总计人数<%d>---总利润为:%d;\n", i, j, z, que.boat[0]._count, que.boat[1]._count, que.boat[2]._count, end_person, person_number, all_monery);
+						fwrite(&buffer, strlen(buffer), 1, fp);
+
+					}
+				}
+			}
+		}
+	}
+	system("pause");
 }
 
 int main()
